9b750cb0b02ace1deb686fa5834f16b9052adc26
Aisha <az-hasan@hotmail.com>
2016-06-07 17:37:13 +0300

    Able to send POST request to http://httpbin.org/post - at least no exceptions given. Added Comments. POST to HTTPS still needs to be implemented (gives SSL handshake error). (Maybe use volley to make things simpler?)

--- app/src/main/java/az/hasan/myfirstapp/MyActivity.java
+++ app/src/main/java/az/hasan/myfirstapp/MyActivity.java
@@ -14,28 +14,37 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
-import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.net.Socket;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLHandshakeException;
 
 public class MyActivity extends AppCompatActivity {
 
+    private EditText newTextMessage;
+    private static final String DEBUG_TAG = "HttpExample";
+
     /** called when "Send New Text Message" button is pressed
-     *  Gets text from box labelled "enter_new_text"**/
+     *  Gets text from box labelled "enter_new_text"
+     *
+     *  Checks for device connectivity. Calls thread to send the text if there is
+     *  connectivity
+     *  **/
     public void onClickSendMessage(View view) {
-        String message;
-        String url = "https://10.0.2.2:8000";
+        String message = "";
+        //String url = "https://10.0.2.2:8000";
+        String url = "http://httpbin.org/post";
         int len = 0;
-        EditText newTextMessage;
+
 
         //Intent intent = new Intent(this, DisplayMessageActivity.class);
         newTextMessage = (EditText) findViewById(R.id.enter_new_text);
@@ -50,15 +59,29 @@ public class MyActivity extends AppCompatActivity {
             SendData mySendData = new SendData();
             mySendData.execute(message, url);
         }else{
-            System.out.println("Not Connected to Network");
+            newTextMessage.setText("Not Connected to Network");
         }
     }
 
-    private class SendData extends AsyncTask<String, Void, Void>{
+    /* Implements background thread class to send data to a given URL
+    *
+    * */
+    private class SendData extends AsyncTask<String, Void, String>{
 
+        /** Called by onClickSendMessage. Takes String message & String url.
+         * Opens a connection to URL "url", and sends "message" using POST
+         *
+         * @param params - can be accessed by param[0], param[1]
+         * @param[0] - "message" passed to method by onClickSendMessage
+         * @param[0] - "url" to send message to, passed to method by onClickSendMessage
+         *      as String
+         * @return  - String value is returned. Explanation of exceptions are also
+         *      returned as String. This return value is used by "onPostExecute"
+         *
+         * **/
         @Override
-        protected Void doInBackground(String... params) {
-            HttpURLConnection myHttpConn = null;
+        protected String doInBackground(String... params) {
+            HttpURLConnection myHttpsConn = null;
             int len = 0;
             OutputStreamWriter outWriter = null;
             String message = params[0];
@@ -69,39 +92,50 @@ public class MyActivity extends AppCompatActivity {
             //define URL and HttpConnection, open connection to URL
             try {
                 myURL = new URL(url);
-                myHttpConn = (HttpURLConnection) myURL.openConnection();
+                myHttpsConn = (HttpURLConnection) myURL.openConnection();
 
                 len = message.length();
-                System.out.println("msg len: " + len + "to send to URL: " + myURL);
+                System.out.println("msg len: [" + len + "] to send to URL: " + myURL);
 
-                myHttpConn.setDoOutput(true);
-                myHttpConn.setRequestMethod("POST");
-                myHttpConn.setFixedLengthStreamingMode(len);
+                myHttpsConn.setDoOutput(true);
+                myHttpsConn.setRequestMethod("POST");
+                myHttpsConn.setFixedLengthStreamingMode(len);
                 System.out.println("set fixed length streaming mode");
 
-                outWriter = new OutputStreamWriter(myHttpConn.getOutputStream());
+                outWriter = new OutputStreamWriter(myHttpsConn.getOutputStream());
                 outWriter.write(message);
                 outWriter.flush();
                 outWriter.close();
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                if (myHttpConn != null) {
-                    myHttpConn.disconnect();
-                }
+                return "success";
             }
 
-            return null;
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "Malformed URL Exception";
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+                return "Prococol Exception";
+            } catch (SSLHandshakeException e){
+                e.printStackTrace();
+                return ("SSLHandshake Exception");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "IO Exception";
+            } finally {
+                if (myHttpsConn != null) {
+                    myHttpsConn.disconnect();
+                }
+            }
         }
 
+
+        /*sets text field to String returns by doInBackground
+        * Exceptions will be printed to said text box
+        * */
         @Override
-        protected void onPostExecute(Void v){
-            System.out.println("Entered post execution");
+        protected void onPostExecute(String result){
+            newTextMessage.setText(result);
         }
     }
 
